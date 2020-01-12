c7a36624b83e22187d4f53c0ca6ab169da2b9924
Tyler Oalman <tyleroalman@gmail.com>
2020-01-11 22:48:50 -0600

    Add basic searching and case sensitive environment variable

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("missing query"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("missing filename"),
+        };
+
+        let case_sensitive = env::var("CASE_SENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let x: Vec<&str> = contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect();
+
+    x
+}
+
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::process;
+
+use simple_grep::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = simple_grep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
